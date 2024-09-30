1da067044d48b5cd6cd12d7d8945958b54d03691
lampros <labros.karachristos@novidea.gr>
2024-09-30 20:43:46 +0300

    spelling checks

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -162,10 +162,10 @@ export default function Team() {
                         What&apos;s cooler than being part of{" "}<br />
                         <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
                             <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-                                <span className="">Hepheastus</span>
+                                <span className="">Hephaestus</span>
                             </div>
                             <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-                                <span className="">Hepheastus</span>
+                                <span className="">Hephaestus</span>
                             </div>
                         </div>
                     </h2>
